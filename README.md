4cedd4cc97b1964738941c01c8a903737206881b
mikediessner <m.diessner2@newcastle.ac.uk>
2024-05-13 20:08:14 +0100

    Ran new LogEI

--- README.md
+++ README.md
@@ -4,27 +4,27 @@ Bayesian optimisation algorithm for expensive experiments and simulations with
 changing environmental conditions" currently in review.
 
 **Abstract:** Experiments in engineering are typically conducted in controlled
-environments where parameters can be set to any desired value. This assumes that
-the same applies in a real-world setting---an assumption that is often incorrect
-as many experiments are influenced by uncontrollable environmental conditions
-such as temperature, humidity and wind speed. When optimising such experiments,
-the focus should lie on finding optimal values conditionally on these
-uncontrollable variables. This article extends Bayesian optimisation to the
-optimisation of systems in changing environments that include controllable and
-uncontrollable parameters. The extension fits a global surrogate model over all
-controllable and environmental variables but optimises only the controllable
-parameters conditional on measurements of the uncontrollable variables. The
-method is validated on two synthetic test functions and the effects of the noise
-level, the number of the environmental parameters, the parameter fluctuation,
-the variability of the uncontrollable parameters, and the effective domain size
-are investigated. ENVBO, the proposed algorithm resulting from this
-investigation, is applied to a wind farm simulator with eight controllable and
-one environmental parameter. The results show that the algorithm finds solutions
-for the full domain of the environmental variable that are in most cases better
-than optimisation algorithms that only focus on a fixed environmental value
-while using a fraction of their evaluation budget. This makes the proposed
-approach very sample-efficient and cost-effective. An off-the-shelf open-source
-version of ENVBO is available via the NUBO Python package.
+environments where parameters can be set to any desired value. This assumes
+that the same applies in a real-world setting, which is often incorrect as many
+experiments are influenced by uncontrollable environmental conditions such as
+temperature, humidity and wind speed. When optimising such experiments, the
+focus should be finding optimal values conditionally on these uncontrollable
+variables. This article extends Bayesian optimisation to the optimisation of
+systems in changing environments that include controllable and uncontrollable
+parameters. The extension fits a global surrogate model over all controllable
+and environmental variables but optimises only the controllable parameters
+conditional on measurements of the uncontrollable variables. The method is
+validated on two synthetic test functions, and the effects of the noise level,
+the number of environmental parameters, the parameter fluctuation, the
+variability of the uncontrollable parameters, and the effective domain size are
+investigated. ENVBO, the proposed algorithm from this investigation, is applied
+to a wind farm simulator with eight controllable and one environmental
+parameter. ENVBO finds solutions for the entire domain of the environmental
+variable that outperform results from optimisation algorithms that only focus
+on a fixed environmental value in all but one case while using a fraction of
+their evaluation budget. This makes the proposed approach very sample-efficient
+and cost-effective. An off-the-shelf open-source version of ENVBO is available
+via the NUBO Python package.
 
 **Cite:**
 
--- empirical_analysis/scripts/experiments/hartmann.py
+++ empirical_analysis/scripts/experiments/hartmann.py
@@ -13,7 +13,7 @@ dynamic_dims = [5]                          # list of indices for dynamic dimens
 num_starts = 20                             # number of initial starts for the optimiser
 num_samples = 100                           # number of samples for num_starts
 evals = 100                                 # number of evaluations
-runs = 5                                    # number of replicaions
+runs = 30                                   # number of replicaions
 stepsize = [0.05]                           # stepsize of random walk
 num_tests = 25                              # number of test points
 eval_stepsize = 10                          # increments of predictions
@@ -24,61 +24,61 @@ walk_str = "1d"
 
 
 # comparison
-# bm(func, noise, bounds, dynamic_dims, num_starts, num_samples, evals, runs, stepsize, num_tests, eval_stepsize, cores, name, start_str, walk_str)
-# print("Benchmark done.")
+bm(func, noise, bounds, dynamic_dims, num_starts, num_samples, evals, runs, stepsize, num_tests, eval_stepsize, cores, name, start_str, walk_str)
+print("Benchmark done.")
 
-# ei(func, noise, bounds, dynamic_dims, num_starts, num_samples, evals, runs, stepsize, num_tests, eval_stepsize, cores, name, start_str, walk_str)
-# print("Expected improvement done.")
+ei(func, noise, bounds, dynamic_dims, num_starts, num_samples, evals, runs, stepsize, num_tests, eval_stepsize, cores, name, start_str, walk_str)
+print("Expected improvement done.")
 
 logei(func, noise, bounds, dynamic_dims, num_starts, num_samples, evals, runs, stepsize, num_tests, eval_stepsize, cores, name, start_str, walk_str)
 print("Log expected improvement done.")
 
-# ucb(func, noise, bounds, dynamic_dims, num_starts, num_samples, evals, runs, stepsize, num_tests, eval_stepsize, cores, name, start_str, walk_str, 4)
-# print("Upper confidence bound beta=4 done.")
+ucb(func, noise, bounds, dynamic_dims, num_starts, num_samples, evals, runs, stepsize, num_tests, eval_stepsize, cores, name, start_str, walk_str, 4)
+print("Upper confidence bound beta=4 done.")
 
-# ucb(func, noise, bounds, dynamic_dims, num_starts, num_samples, evals, runs, stepsize, num_tests, eval_stepsize, cores, name, start_str, walk_str, 8)
-# print("Upper confidence bound beta=8 done.")
+ucb(func, noise, bounds, dynamic_dims, num_starts, num_samples, evals, runs, stepsize, num_tests, eval_stepsize, cores, name, start_str, walk_str, 8)
+print("Upper confidence bound beta=8 done.")
 
-# ucb(func, noise, bounds, dynamic_dims, num_starts, num_samples, evals, runs, stepsize, num_tests, eval_stepsize, cores, name, start_str, walk_str, 16)
-# print("Upper confidence bound beta=16 done.")
-
-
-# # noise
-# ei(func, 0.025, bounds, dynamic_dims, num_starts, num_samples, evals, runs, stepsize, num_tests, eval_stepsize, cores, f"{name}_noisy_0.025", start_str, walk_str)
-# print("Noise 0.025 done.")
-
-# ei(func, 0.05, bounds, dynamic_dims, num_starts, num_samples, evals, runs, stepsize, num_tests, eval_stepsize, cores, f"{name}_noisy_0.05", start_str, walk_str)
-# print("Noise 0.05 done.")
-
-# ei(func, 0.1, bounds, dynamic_dims, num_starts, num_samples, evals, runs, stepsize, num_tests, eval_stepsize, cores, f"{name}_noisy_0.1", start_str, walk_str)
-# print("Noise 0.1 done.")
+ucb(func, noise, bounds, dynamic_dims, num_starts, num_samples, evals, runs, stepsize, num_tests, eval_stepsize, cores, name, start_str, walk_str, 16)
+print("Upper confidence bound beta=16 done.")
 
 
-# # multiple environmental conditions
-# ei(func, noise, bounds, [3, 5], num_starts, num_samples, evals, runs, [0.10, 0.05], 50, eval_stepsize, cores, f"{name}_multi_2", start_str, "2d")
-# print("2 environmental conditions done.")
+# noise
+ei(func, 0.025, bounds, dynamic_dims, num_starts, num_samples, evals, runs, stepsize, num_tests, eval_stepsize, cores, f"{name}_noisy_0.025", start_str, walk_str)
+print("Noise 0.025 done.")
 
-# ei(func, noise, bounds, [0, 3, 5], num_starts, num_samples, evals, runs, [0.10, 0.10, 0.05], 75, eval_stepsize, cores, f"{name}_multi_3", start_str, "3d")
-# print("3 environmental conditions done.")
+ei(func, 0.05, bounds, dynamic_dims, num_starts, num_samples, evals, runs, stepsize, num_tests, eval_stepsize, cores, f"{name}_noisy_0.05", start_str, walk_str)
+print("Noise 0.05 done.")
 
-
-# # fluctuation
-# ei(func, noise, bounds, dynamic_dims, num_starts, num_samples, evals, runs, [0.1], num_tests, eval_stepsize, cores, f"{name}_stepsize_0.1", start_str, walk_str)
-# print("Fluctuation 0.1 done.")
-
-# ei(func, noise, bounds, dynamic_dims, num_starts, num_samples, evals, runs, [0.25], num_tests, eval_stepsize, cores, f"{name}_stepsize_0.25", start_str, walk_str)
-# print("Fluctuation 0.25 done.")
-
-# ei(func, noise, bounds, dynamic_dims, num_starts, num_samples, evals, runs, [0.5], num_tests, eval_stepsize, cores, f"{name}_stepsize_0.5", start_str, walk_str)
-# print("Fluctuation 0.5 done.")
-
-# ei(func, noise, bounds, dynamic_dims, num_starts, num_samples, evals, runs, [1.0], num_tests, eval_stepsize, cores, f"{name}_stepsize_1.0", start_str, walk_str)
-# print("Fluctuation 1.0 done.")
+ei(func, 0.1, bounds, dynamic_dims, num_starts, num_samples, evals, runs, stepsize, num_tests, eval_stepsize, cores, f"{name}_noisy_0.1", start_str, walk_str)
+print("Noise 0.1 done.")
 
 
-# # parameter variability
-# ei(func, noise, bounds, [2], num_starts, num_samples, evals, runs, stepsize, num_tests, eval_stepsize, cores, f"{name}_variability_low", start_str, walk_str)
-# print("Low variability done.")
+# multiple environmental conditions
+ei(func, noise, bounds, [3, 5], num_starts, num_samples, evals, runs, [0.10, 0.05], 50, eval_stepsize, cores, f"{name}_multi_2", start_str, "2d")
+print("2 environmental conditions done.")
 
-# ei(func, noise, bounds, [0], num_starts, num_samples, evals, runs, stepsize, num_tests, eval_stepsize, cores, f"{name}_variability_high", start_str, walk_str)
-# print("High variability done.")
+ei(func, noise, bounds, [0, 3, 5], num_starts, num_samples, evals, runs, [0.10, 0.10, 0.05], 75, eval_stepsize, cores, f"{name}_multi_3", start_str, "3d")
+print("3 environmental conditions done.")
+
+
+# fluctuation
+ei(func, noise, bounds, dynamic_dims, num_starts, num_samples, evals, runs, [0.1], num_tests, eval_stepsize, cores, f"{name}_stepsize_0.1", start_str, walk_str)
+print("Fluctuation 0.1 done.")
+
+ei(func, noise, bounds, dynamic_dims, num_starts, num_samples, evals, runs, [0.25], num_tests, eval_stepsize, cores, f"{name}_stepsize_0.25", start_str, walk_str)
+print("Fluctuation 0.25 done.")
+
+ei(func, noise, bounds, dynamic_dims, num_starts, num_samples, evals, runs, [0.5], num_tests, eval_stepsize, cores, f"{name}_stepsize_0.5", start_str, walk_str)
+print("Fluctuation 0.5 done.")
+
+ei(func, noise, bounds, dynamic_dims, num_starts, num_samples, evals, runs, [1.0], num_tests, eval_stepsize, cores, f"{name}_stepsize_1.0", start_str, walk_str)
+print("Fluctuation 1.0 done.")
+
+
+# parameter variability
+ei(func, noise, bounds, [2], num_starts, num_samples, evals, runs, stepsize, num_tests, eval_stepsize, cores, f"{name}_variability_low", start_str, walk_str)
+print("Low variability done.")
+
+ei(func, noise, bounds, [0], num_starts, num_samples, evals, runs, stepsize, num_tests, eval_stepsize, cores, f"{name}_variability_high", start_str, walk_str)
+print("High variability done.")
