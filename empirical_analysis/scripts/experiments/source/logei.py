6b3a0eb916ed0d81335ca066b901d500e2864ddb
mikediessner <m.diessner2@newcastle.ac.uk>
2024-05-13 13:11:59 +0100

    Different logei implementation

--- empirical_analysis/scripts/experiments/source/logei.py
+++ empirical_analysis/scripts/experiments/source/logei.py
@@ -3,12 +3,76 @@ from torch import Tensor
 from gpytorch.models import GP
 from nubo.acquisition.acquisition_function import AcquisitionFunction
 import math
+from math import pi
 import numpy as np
 from nubo.models import GaussianProcess, fit_gp
 from gpytorch.likelihoods import GaussianLikelihood
 from nubo.algorithms import _cond_optim
 from source.helper import make_header, update_env
 import json
+
+
+_neg_inv_sqrt2 = -(2**-0.5)
+_log_sqrt_pi_div_2 = math.log(math.pi / 2) / 2
+_log_sqrt_2pi = math.log(2 * pi) / 2
+_log2 = math.log(2)
+_inv_sqrt_2pi = 1 / math.sqrt(2 * pi)
+
+
+def _ei_helper(u: Tensor) -> Tensor:
+    """Computes phi(u) + u * Phi(u), where phi and Phi are the standard normal
+    pdf and cdf, respectively. This is used to compute Expected Improvement.
+    """
+    return phi(u) + u * Phi(u)
+
+
+def _log_abs_u_Phi_div_phi(u: Tensor) -> Tensor:
+    """Computes log(abs(u) * Phi(u) / phi(u)), where phi and Phi are the normal pdf
+    and cdf, respectively. The function is valid for u < 0.
+
+    NOTE: In single precision arithmetic, the function becomes numerically unstable for
+    u < -1e3. For this reason, a second branch in _log_ei_helper is necessary to handle
+    this regime, where this function approaches -abs(u)^-2 asymptotically.
+
+    The implementation is based on the following implementation of the logarithm of
+    the scaled complementary error function (i.e. erfcx). Since we only require the
+    positive branch for _log_ei_helper, _log_abs_u_Phi_div_phi does not have a branch,
+    but is only valid for u < 0 (so that _neg_inv_sqrt2 * u > 0).
+
+        def logerfcx(x: Tensor) -> Tensor:
+            return torch.where(
+                x < 0,
+                torch.erfc(x.masked_fill(x > 0, 0)).log() + x**2,
+                torch.special.erfcx(x.masked_fill(x < 0, 0)).log(),
+        )
+
+    Further, it is important for numerical accuracy to move u.abs() into the
+    logarithm, rather than adding u.abs().log() to logerfcx. This is the reason
+    for the rather complex name of this function: _log_abs_u_Phi_div_phi.
+    """
+    # get_constants_like allocates tensors with the appropriate dtype and device and
+    # caches the result, which improves efficiency.
+    a, b = _neg_inv_sqrt2, _log_sqrt_pi_div_2
+    return torch.log(torch.special.erfcx(a * u) * u.abs()) + b
+
+
+def log1mexp(x: Tensor) -> Tensor:
+    """Numerically accurate evaluation of log(1 - exp(x)) for x < 0.
+    See [Maechler2012accurate]_ for details.
+    """
+    log2 = _log2
+    is_small = -log2 < x  # x < 0
+    return torch.where(
+        is_small,
+        (-x.expm1()).log(),
+        (-x.exp()).log1p(),
+    )
+
+
+def log_phi(x: Tensor) -> Tensor:
+    r"""Logarithm of standard normal pdf"""
+    log_sqrt_2pi, neg_half = _log_sqrt_2pi, -0.5
+    return neg_half * x.square() - log_sqrt_2pi
 
 
 class LogExpectedImprovement(AcquisitionFunction):
@@ -23,26 +87,55 @@ class LogExpectedImprovement(AcquisitionFunction):
         self.gp = gp
         self.y_best = y_best
     
-    def log_h(self, z):
+    def _log_ei_helper(self, u: Tensor) -> Tensor:
+        """Accurately computes log(phi(u) + u * Phi(u)) in a differentiable manner for u in
+        [-10^100, 10^100] in double precision, and [-10^20, 10^20] in single precision.
+        Beyond these intervals, a basic squaring of u can lead to floating point overflow.
+        In contrast, the implementation in _ei_helper only yields usable gradients down to
+        u ~ -10. As a consequence, _log_ei_helper improves the range of inputs for which a
+        backward pass yields usable gradients by many orders of magnitude.
+        """
+        if not (u.dtype == torch.float32 or u.dtype == torch.float64):
+            raise TypeError(
+                f"LogExpectedImprovement only supports torch.float32 and torch.float64 "
+                f"dtypes, but received {u.dtype = }."
+            )
+        # The function has two branching decisions. The first is u < bound, and in this
+        # case, just taking the logarithm of the naive _ei_helper implementation works.
+        bound = -1
+        u_upper = u.masked_fill(u < bound, bound)  # mask u to avoid NaNs in gradients
+        log_ei_upper = _ei_helper(u_upper).log()
 
-        c_1 = torch.log(torch.tensor(2)*torch.pi)/2
-        c_2 = torch.log(torch.tensor(torch.pi)/2)/2
+        # When u <= bound, we need to be more careful and rearrange the EI formula as
+        # log(phi(u)) + log(1 - exp(w)), where w = log(abs(u) * Phi(u) / phi(u)).
+        # To this end, a second branch is necessary, depending on whether or not u is
+        # smaller than approximately the negative inverse square root of the machine
+        # precision. Below this point, numerical issues in computing log(1 - exp(w)) occur
+        # as w approaches zero from below, even though the relative contribution to log_ei
+        # vanishes in machine precision at that point.
+        neg_inv_sqrt_eps = -1e6 if u.dtype == torch.float64 else -1e3
 
-        def log1mexp(x):
-            if -torch.log(torch.tensor(2)) < x:
-                return torch.log(-torch.expm1(x))
-            elif -torch.log(torch.tensor(2)) >= x:
-                return torch.log1p(-torch.exp(x))
+        # mask u for to avoid NaNs in gradients in first and second branch
+        u_lower = u.masked_fill(u > bound, bound)
+        u_eps = u_lower.masked_fill(u < neg_inv_sqrt_eps, neg_inv_sqrt_eps)
+        # compute the logarithm of abs(u) * Phi(u) / phi(u) for moderately large negative u
+        w = _log_abs_u_Phi_div_phi(u_eps)
 
-        def logerfcx(z):
-                return torch.log(torch.special.erfcx(-z/torch.sqrt(torch.tensor(2)))*torch.abs(z))
-        
-        if z > -1:
-            return (phi(z) + z * Phi(z)).log()
-        elif z <= -1:
-            mid_part = logerfcx(z) + c_2
-            outer_part = log1mexp(mid_part)
-            return -(z**2)/2 - c_1 + outer_part
+        # 1) Now, we use a special implementation of log(1 - exp(w)) for moderately
+        # large negative numbers, and
+        # 2) capture the leading order of log(1 - exp(w)) for very large negative numbers.
+        # The second special case is technically only required for single precision numbers
+        # but does "the right thing" regardless.
+        log_ei_lower = log_phi(u) + (
+            torch.where(
+                u > neg_inv_sqrt_eps,
+                log1mexp(w),
+                # The contribution of the next term relative to log_phi vanishes when
+                # w_lower << eps but captures the leading order of the log1mexp term.
+                -2 * u_lower.abs().log(),
+            )
+        )
+        return torch.where(u > bound, log_ei_upper, log_ei_lower)
         
     def eval(self, x: Tensor) -> Tensor:
 
@@ -62,19 +155,21 @@ class LogExpectedImprovement(AcquisitionFunction):
 
         # compute log Expected Improvement
         z = (mean - self.y_best)/std
-        ei = self.log_h(z) + torch.log(std)
+        ei = self._log_ei_helper(z) + std.log()
 
         return -ei
 
 
 def phi(x: Tensor) -> Tensor:
     r"""Standard normal PDF."""
-    return 1 / math.sqrt(2 * math.pi) * (-0.5 * x.square()).exp()
+    inv_sqrt_2pi, neg_half = _inv_sqrt_2pi, -0.5
+    return inv_sqrt_2pi * (neg_half * x.square()).exp()
 
 
 def Phi(x: Tensor) -> Tensor:
     r"""Standard normal CDF."""
-    return 0.5 * torch.erfc(-(1 / math.sqrt(2)) * x)
+    log_sqrt_2pi, neg_half = _log_sqrt_2pi, -0.5
+    return neg_half * x.square() - log_sqrt_2pi
 
 
 def envbo_step(x_train, y_train, bounds, env_values, env_dims, num_starts, num_samples):
